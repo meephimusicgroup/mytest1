9f841a05707ad27b729e65e881c4f90604f7d170
Артур Мартемьянов <arturmartemanov@192.168.0.10>
2023-11-19 23:10:35 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#ето короче мой первый проект
+#print("Результат:", 5, 30, 60, sep="", end="\n")
+#print("Second Line")
+
+from turtle import *
+
+pensize(10)
+
+while True:
+    forward(1)
+    left(1)
